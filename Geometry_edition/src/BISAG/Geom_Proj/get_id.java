d0c471f3d1f70be5b70373936352386a1a98532c
surgeonofdeath <surgeonofdeath@users.noreply.github.com>
2017-07-10 16:57:56 +0530

    Updated last minute changes

--- Geometry_edition/src/BISAG/Geom_Proj/get_id.java
+++ Geometry_edition/src/BISAG/Geom_Proj/get_id.java
@@ -1,11 +1,6 @@
 package BISAG.Geom_Proj;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,76 +8,79 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 /**
- * Servlet implementation class edit_feature
+ * Servlet implementation class return_json
  */
-@WebServlet("/edit_feature")
+@WebServlet("/delete_feature")
 public class get_id extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {}
 	
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String usename = request.getParameter("top");
-		String id= request.getParameter("id");
-		System.out.println(id);
-		System.out.println(usename);
-		get_id ef = new get_id();
-		try {
-			if(!id.equals("-1")){
-			ef.update_db(usename, id);
-			}
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		String urls = request.getParameter("top");
+		urls = urls.replace("[]","&");
+		urls = "http://"+urls;
+		System.out.println(urls);
+		get_id rj = new get_id();
+		URL url = new URL(urls);
+		int id = rj.getid(url);
+		out.println(id);
 	}
 	
-	public void update_db(String route, String id) throws ClassNotFoundException, SQLException{
-		Connection conn = null;
-		ResultSet rs = null;
-		String name = null;
-		String notes = null;
-		Class.forName("org.postgresql.Driver");
-		String user = "postgres";
-		String password = "postgresql";
-		String url = "jdbc:postgresql://localhost:5432/postgres";
-		try {
-			conn = DriverManager.getConnection(url, user, password);
-			System.out.println(conn);
-		} catch (SQLException e) {
-			System.out.println(e);
-		}
-		String query="select * from features_test where id="+id;
-		Statement stmt = conn.createStatement();
-		System.out.println(query);
-		try{
-			rs = stmt.executeQuery(query);
-		}catch(Exception e){}
-		while(rs.next()){
-			name = rs.getString("name");
-			notes = rs.getString("notes");
-		}
-		
-		query = "delete from features_test where id="+id+";";
-		stmt = conn.createStatement();
-		System.out.println(query);
-		try{
-			rs = stmt.executeQuery(query);
-		}catch(Exception e){}
-		
-		query = "insert into features_test values('"+ name +"', '"+ notes +"', '"+ id +"', ST_GeomFromText('"+ route +"', 4326));";
-		stmt = conn.createStatement();
-		System.out.println(query);
-		try{
-			rs = stmt.executeQuery(query);
-		}catch(Exception e){}
-		while(rs.next()){
-			name = rs.getString("name");
-			notes = rs.getString("notes");
-		}
+	public int getid(URL url){
+	    InputStream is = null;
+	    BufferedReader br;
+	    String line;
+	    String id_s = null;
+	    int id=-1;
+	    try {
+	        is = url.openStream();  // throws an IOException
+	        br = new BufferedReader(new InputStreamReader(is));
+
+	        while ((line = br.readLine()) != null) {
+	        	 Pattern p = Pattern.compile(">\\d+<");
+	        	 Matcher m = p.matcher(line);
+	        	 if(m.find())
+	        	    if((m.group().subSequence(1, m.group().length()-1)).length()!=3)
+	        	    		{
+	        	    			id_s = (String)m.group().subSequence(1, m.group().length()-1);
+	        	    		}
+	        	}
+	        System.out.println(id_s);
+	    } catch (MalformedURLException mue) {
+	         mue.printStackTrace();
+	    } catch (IOException ioe) {
+	    	System.out.println("IO EXCEPTION");
+	         ioe.printStackTrace();
+	    } finally {
+	        try {
+	            if (is != null) is.close();
+	        } catch (IOException ioe) {
+	            // nothing to see here
+	        }
+	    }
+	    try{
+	    	id = Integer.parseInt(id_s);
+	    }catch(Exception e){}
+	    
+		return id;
 	}
+	
 }
